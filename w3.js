db807db06f2d3b2168cf491ff4266ee0d0e16a76
Ahmed Ihab <ahmedinouh@gmail.com>
2024-07-04 02:32:54 +0300

    noting

--- w3.js
+++ w3.js
@@ -1,28 +1,30 @@
-let today = new Date();
-let day = today.getDay();
+// 1. let today = new Date();
+// let day = today.getDay();
 
-switch(day) {
-    case 0:
-        console.log("Today is Sunday");
-        break;
-    case 1:
-        console.log("Today is Monday");
-        break;
-    case 2:
-        console.log("Today is Tuesday");
-        break;
-    case 3:
-        console.log("Today is ttt");
-        break;
-    case 4:
-        console.log("Today is Thursday");
-        break;
-    case 5:
-        console.log("Today is Friday");
-        break;
-    case 6:
-        console.log("Today is Saturday");
-        break;
-    default:
-        console.log("Invalid day");
-}
+// switch(day) {
+//     case 0:
+//         console.log("Today is Sunday");
+//         break;
+//     case 1:
+//         console.log("Today is Monday");
+//         break;
+//     case 2:
+//         console.log("Today is Tuesday");
+//         break;
+//     case 3:
+//         console.log("Today is ttt");
+//         break;
+//     case 4:
+//         console.log("Today is Thursday");
+//         break;
+//     case 5:
+//         console.log("Today is Friday");
+//         break;
+//     case 6:
+//         console.log("Today is Saturday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
